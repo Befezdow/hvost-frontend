ac4eaab35dcd62d3783b2250d56e21d0b11b2916
Evgeniy Simakov <befezd@gmail.com>
2024-05-20 20:13:08 +0300

    Fixed login layout

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,47 @@
-import { Root } from "./styled";
-import { LoginForm } from "../../components/loginForm";
+import { Form } from "antd";
+import {
+  Root,
+  LoginCard,
+  Header,
+  LoginDisclaimer,
+  LoginForm,
+  InputField,
+  SubmitButton,
+} from "./styled";
 
 export const LoginPage = () => {
-	return (
-		<Root>
-			<LoginForm/>
-		</Root>
-	);
+  const onSubmit = (values) => {
+    console.log("Success:", values);
+  };
+
+  return (
+    <Root>
+      <LoginCard>
+        <Header>Войти</Header>
+        <LoginDisclaimer>
+          Внимание! Авторизация на сайте доступна только для зарегистрированных
+          организаций. Если вы являетесь представителем организации для помощи
+          животным и хотите зарегистрироваться - отправьте заявку на почтовый
+          адрес mariya161617@yandex.ru.
+        </LoginDisclaimer>
+        <LoginForm onFinish={onSubmit}>
+          <Form.Item
+            name="login"
+            rules={[{ required: true, message: "Обязательное поле" }]}
+          >
+            <InputField type="email" placeholder="Логин" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Обязательное поле" }]}
+          >
+            <InputField type="password" placeholder="Пароль" />
+          </Form.Item>
+          <Form.Item>
+            <SubmitButton block htmlType="submit">Войти</SubmitButton>
+          </Form.Item>
+        </LoginForm>
+      </LoginCard>
+    </Root>
+  );
 };
--- src/pages/login/styled.js
+++ src/pages/login/styled.js
@@ -1,8 +1,70 @@
 import styled from "styled-components";
+import { Button, Input, Form } from "antd";
 
 export const Root = styled.div`
-	height: calc(100vh - 106px);
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  height: calc(100vh - 106px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoginCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  max-width: 500px;
+  min-width: 500px;
+  padding: 40px;
+  border-radius: 14px;
+  background-color: #fff;
+  box-shadow: 0 4px 8px 0 rgba(100, 100, 100, 0.2),
+    0 6px 20px 0 rgba(100, 100, 100, 0.2);
+`;
+
+export const LoginForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+`;
+
+export const Header = styled.div`
+  font-size: 36px;
+  color: #000;
+  text-align: center;
+`;
+
+export const LoginDisclaimer = styled.div`
+  padding: 20px;
+  background-color: #cce5ff;
+  color: #004085;
+  border: 1px solid;
+  border-color: #b8daff;
+  border-radius: 14px;
+  font-size: 16px;
+  text-align: justify;
+`;
+
+export const InputField = styled(Input)`
+  height: 50px;
+  border: none;
+  outline: none;
+  border: 1px solid black;
+  border-radius: 14px;
+  padding: 0 20px;
+  font-size: 20px;
+
+  &::placeholder {
+    font-size: 20px;
+  }
+`;
+
+export const SubmitButton = styled(Button)`
+  background-color: black;
+  color: white;
+  font-size: 20px;
+  border-radius: 14px;
+
+  &:hover {
+    background-color: #474747;
+  }
 `;
